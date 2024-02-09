e8cf143d3ed8a665c4400b3c70c54ee7e69a892c
Mahdi Mahdawi <mj.mahdawi@gmail.com>
2024-02-09 02:10:12 -0600

    Deploy index.html to GitHub Pages

---
+++ static/precache-manifest.740a8f13480a715a56345bec1d928d2e.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354b39b6ee4ba2cfb72cbb086f92030b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6689a714fda9558b79f2",
+    "url": "/static/css/2.c216443c.chunk.css"
+  },
+  {
+    "revision": "7fcc163dac76563ae073",
+    "url": "/static/css/main.1b4049ce.chunk.css"
+  },
+  {
+    "revision": "6689a714fda9558b79f2",
+    "url": "/static/js/2.4d66497c.chunk.js"
+  },
+  {
+    "revision": "89d43ad9a4e13acb6b29fb12d3eb4028",
+    "url": "/static/js/2.4d66497c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df512e81a9137ce4531e",
+    "url": "/static/js/3.92109ec7.chunk.js"
+  },
+  {
+    "revision": "7fcc163dac76563ae073",
+    "url": "/static/js/main.4b84f105.chunk.js"
+  },
+  {
+    "revision": "3d93b4bfc611a8b57bb0",
+    "url": "/static/js/runtime-main.809ee001.js"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "f750170aaddf4e5f885a27d391a311d9",
+    "url": "/static/media/arrow1.f750170a.svg"
+  },
+  {
+    "revision": "9070490babe4d1aa7f077b104555b363",
+    "url": "/static/media/arrow2.9070490b.svg"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "bfe6d1a6d579d73c0c625ec0afe2855b",
+    "url": "/static/media/header-img.bfe6d1a6.svg"
+  },
+  {
+    "revision": "788738273b1a1eb1be363be36a7359a3",
+    "url": "/static/media/logo.78873827.svg"
+  },
+  {
+    "revision": "b5d6bf8df246077145e036d6e12e7e11",
+    "url": "/static/media/logo.b5d6bf8d.svg"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "70af9240bb95dd2d65f6bf9f09591507",
+    "url": "/static/media/project-img1.70af9240.png"
+  },
+  {
+    "revision": "98b3bc237950d6cb1a1e49f75372ae36",
+    "url": "/static/media/project-img2.98b3bc23.png"
+  },
+  {
+    "revision": "08e5b04289e11fe1c0776cef35c4bcf2",
+    "url": "/static/media/project-img3.08e5b042.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
