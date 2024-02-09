834a4be1a4ccacdd41af7dc392517163e13da2e9
Mahdi Mahdawi <mj.mahdawi@gmail.com>
2024-02-09 14:56:13 -0600

    new file move-index created

--- move-index.js
+++ move-index.js
@@ -3,14 +3,13 @@ const path = require('path');
 
 // Define paths
 const buildPath = path.resolve(__dirname, 'build');
-const indexPath = path.resolve(buildPath, 'index.html');
-const newIndexLocation = path.resolve(__dirname, 'index.html');
+const filesToMove = fs.readdirSync(buildPath);
 
-// Move index.html to the root directory
-fs.rename(indexPath, newIndexLocation, (err) => {
-    if (err) {
-        console.error('Error moving index.html:', err);
-    } else {
-        console.log('index.html moved to the root directory successfully.');
-    }
+// Move files to the root directory
+filesToMove.forEach((file) => {
+    const sourcePath = path.resolve(buildPath, file);
+    const destinationPath = path.resolve(__dirname, file);
+
+    fs.renameSync(sourcePath, destinationPath);
+    console.log(`${file} moved to the root directory.`);
 });
